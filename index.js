2a2c320f426ced2c009bac1daccdf28650b4dc87
Arthur Cavalcante <linsarthur02@gmail.com>
2025-06-10 15:58:32 -0300

    rota de login e rota protegida

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import cors from "cors";
 import express from "express";
+import { login } from "./src/controller/usuariosController.js";
 import { usuariosRoutes } from "./src/routes/usuariosRoutes.js";
+import { verificarToken } from "./src/utils/index.js";
 const app = express();
 const port = 8000;
 
@@ -9,7 +11,11 @@ app.use(cors());
 app.use(express.json());
 
 
-app.use("/usuarios", usuariosRoutes)
+app.post("/login", async (req, res) => {
+    res.send(await login(req.body))
+})
+
+app.use("/usuarios", verificarToken, usuariosRoutes)
 
 app.use((req, res) => {
     res.status(404).send("Rota não encontrada")
