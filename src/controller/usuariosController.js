43c8c547b01dd6b81a7ca4d54e8c2bec9c03588f
Arthur Cavalcante <linsarthur02@gmail.com>
2025-06-10 14:55:09 -0300

    tratativa de erro

--- src/controller/usuariosController.js
+++ src/controller/usuariosController.js
@@ -2,48 +2,68 @@ import bcrypt from "bcrypt";
 import { prisma } from "../services/index.js";
 
 export async function buscarUsuarios() {
-    return await prisma.usuarios.findMany();
+    try {
+        return await prisma.usuarios.findMany();
+    } catch (error) {
+        return error.message
+    }
 }
 
 export async function buscarUsuarioPorId(id) {
-    return await prisma.usuarios.findUnique({
-        where: {
-            usuario_id: Number(id)
-        }
-    });
+    try {
+        return await prisma.usuarios.findUnique({
+            where: {
+                usuario_id: Number(id)
+            }
+        });
+    } catch (error) {
+        return error.message
+    }
 }
 
 export async function criarUsuario(dados) {
-    const saltRounds = 10
-    const senhaCriptografada = await bcrypt.hash(dados.usuario_senha, saltRounds)
-    const cpfCriptografado = await bcrypt.hash(dados.usuario_cpf, saltRounds)
-    const cnpjCriptografado = await bcrypt.hash(dados.usuario_cnpj, saltRounds)
-    return await prisma.usuarios.create({
-        data: {
-            ...dados,
-            usuario_senha: senhaCriptografada,
-            usuario_cpf: cpfCriptografado,
-            usuario_cnpj: cnpjCriptografado
-        }
+    try {
+        const saltRounds = 10
+        const senhaCriptografada = await bcrypt.hash(dados.usuario_senha, saltRounds)
+        const cpfCriptografado = await bcrypt.hash(dados.usuario_cpf, saltRounds)
+        const cnpjCriptografado = await bcrypt.hash(dados.usuario_cnpj, saltRounds)
+        return await prisma.usuarios.create({
+            data: {
+                ...dados,
+                usuario_senha: senhaCriptografada,
+                usuario_cpf: cpfCriptografado,
+                usuario_cnpj: cnpjCriptografado
+            }
 
-    });
+        });
+    } catch (error) {
+        return error.message
+    }
 }
 
 export async function editarUsuarios(id, dados) {
-    return await prisma.usuarios.update({
-        where: {
-            usuario_id: Number(id)
-        },
-        data: dados
-    });
+    try {
+        return await prisma.usuarios.update({
+            where: {
+                usuario_id: Number(id)
+            },
+            data: dados
+        });
+    } catch (error) {
+        return error.message
+    }
 }
 
 export async function deletarUsuarios(id) {
-    return await prisma.usuarios.delete({
-        where: {
-            usuario_id: Number(id)
-        }
-    });
+    try {
+        return await prisma.usuarios.delete({
+            where: {
+                usuario_id: Number(id)
+            }
+        });
+    } catch (error) {
+        return error.message
+    }
 }
 
 
